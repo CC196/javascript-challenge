b4e0784d50bd9be87a701ab58c40945ec6e3a2be
CC196 <ccai.yangling@gmail.com>
2020-09-13 00:11:13 -0700

    find the sightings

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,58 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tablebody = d3.select("tbody");
+
+tableData.forEach(function(sighting){
+    var new_row = tablebody.append("tr")
+    new_row.append("td").text(sighting.datetime);
+    new_row.append("td").text(sighting.city);
+    new_row.append("td").text(sighting.state);
+    new_row.append("td").text(sighting.country);
+    new_row.append("td").text(sighting.shape);
+    new_row.append("td").text(sighting.durationMinutes);
+    new_row.append("td").text(sighting.comments);
+});
+
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+button.on("click", filtertable);
+form.on("submit", filtertable);
+
+function filtertable(){
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    var ufo = tableData.filter(function(sighting){
+        return sighting.datetime == inputValue
+    });
+
+    tablebody.html("")
+    if (ufo.length === 0){
+        var new_row = tablebody.append("tr")
+        new_row.append("td").text("NaN");
+        new_row.append("td").text("NaN");
+        new_row.append("td").text("NaN");
+        new_row.append("td").text("NaN");
+        new_row.append("td").text("NaN");
+        new_row.append("td").text("NaN");
+        new_row.append("td").text("NaN");
+    }else{
+        ufo.forEach(function(sighting){
+            
+            var new_row = tablebody.append("tr")
+            new_row.append("td").text(sighting.datetime);
+            new_row.append("td").text(sighting.city);
+            new_row.append("td").text(sighting.state);
+            new_row.append("td").text(sighting.country);
+            new_row.append("td").text(sighting.shape);
+            new_row.append("td").text(sighting.durationMinutes);
+            new_row.append("td").text(sighting.comments);
+        });
+    };
+
+};
